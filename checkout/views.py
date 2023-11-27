efabe5fae12abaae5a199fca1cb0460060adf98b
BrendanCooney <advicast@gmail.com>
2023-11-27 07:08:11 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51IomalBdmMK809BzitNj22299VnjbdAs2wX5tbhHg75WhPJY22qVaI9WS6Z5AGdlmCCYkbgNMZIEoMBjUz14Lr3B00Hf0ORsMZ',
+        'client_secret': 'test_client_secret'
     }
 
     return render(request, template, context)
