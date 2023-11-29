df235b4d1673d81958f97b254c78e49fc62ff3ae
BrendanCooney <advicast@gmail.com>
2023-11-29 08:27:48 +0000

    updated settings for media deployment

--- devwear/settings.py
+++ devwear/settings.py
@@ -190,6 +190,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'brendancooney-devwear'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -206,6 +212,11 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+    # Cache Control
+    AWS_S3_OBJECT_PERAMETERS = {
+        'Expires' : ''
+    }
 
 
 
