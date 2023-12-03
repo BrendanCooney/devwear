74241a6dbceabaf0f16762757a4e7b5c3695b071
BrendanCooney <advicast@gmail.com>
2023-12-03 13:57:33 +0000

    made changed to debug and logo font

--- devwear/settings.py
+++ devwear/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'True' in os environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-brendancooney-devwear-0ykx5pim6ib.ws-eu106.gitpod.io','devwear.herokuapp.com', 'localhost', 'devwear-0c4ac54770df.herokuapp.com' ]
 
