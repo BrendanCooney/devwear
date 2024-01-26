0c9a6cad6caa9e7109275c7a5ed210d100ce81a6
BrendanCooney <advicast@gmail.com>
2024-01-26 01:20:08 +0000

    added changes to the post detail view

--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()\
             .filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
-
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
@@ -37,8 +38,5 @@ class Post(models.Model):
             models.Index(fields=['-publish']),
         ]
 
-
     def __str__(self):
         return self.title
-
-
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def post_list(request):
     posts = Post.published.all()
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
+
 
 def post_detail(request, id):
     post = get_object_or_404(Post,
@@ -14,4 +16,5 @@ def post_detail(request, id):
 
     return render(request,
            'blog/post/detail.html',
-           {'post':post})   
+           {'post': post})
+ 
