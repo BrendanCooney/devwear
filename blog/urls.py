4db145ed3788e9f15d0b559402c3086df6c77fe9
BrendanCooney <advicast@gmail.com>
2024-03-02 19:04:11 +0000

    added email post form views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/',
+    views.post_share, name='post_share'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -14,6 +16,36 @@ def post_detail(request, id):
                              id=id,
                              status=Post.Status.PUBLISHED)
 
+
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'advicast@gmail.com',
+                [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form,
+                                                        'sent': sent})
